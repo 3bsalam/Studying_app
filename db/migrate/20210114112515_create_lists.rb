5580cb7c67a51ed5c7b8996e416eb4efa7ff8ad0
root <root@vmi421823.contaboserver.net>
2021-01-14 12:35:35 +0100

    added lists table to db

---
+++ db/migrate/20210114112515_create_lists.rb
@@ -0,0 +1,9 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.belongs_to :user
+      t.string :title
+      t.timestamps
+    end
+  end
+end
